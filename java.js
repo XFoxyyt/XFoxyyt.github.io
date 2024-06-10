f7b38faf05f1a8fed136990d1778890f1b389b95
XFoxyyt <148640004+XFoxyyt@users.noreply.github.com>
2024-06-10 21:29:08 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
-fetch('https://games.roblox.com/v1/games/' + gameId + '/servers')
+fetch('https://games.roblox.com/v1/games/' + 17807010713 + '/servers')
   .then(response => response.json())
   .then(data => {
     const playerCount = data.data[0].playing;
-    document.getElementById('player-count').innerHTML = `Players online: ${playerCount}`;
+    document.getElementById('player-count').innerHTML = `Players online: ${0}`;
   })
   .catch(error => console.error('Error:', error));
